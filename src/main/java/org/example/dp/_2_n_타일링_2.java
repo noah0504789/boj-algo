5e3e864fb4e265e959efcc36240a8f8477819d97
noah0504789 <noah0504@naver.com>
2023-11-10 18:04:46 +0900

    [feat/dp] 2*n 타일링 2

---
+++ src/main/java/org/example/dp/_2_n_타일링_2.java
@@ -0,0 +1,33 @@
+package org.example.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 2*n 크기의 직사각형을 1*2, 2*1, 2*2 타일로 채우는 방법의 수를 구하라
+public class _2_n_타일링_2 {
+
+    static int N;
+    static int[] dp; // j열까지 최소 타월 수
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i-1] + 2*dp[i-2])%10007;
+        }
+
+        System.out.println(dp[N]);
+    }
+
+    private static void init() throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        dp = new int[1001];
+    }
+}
