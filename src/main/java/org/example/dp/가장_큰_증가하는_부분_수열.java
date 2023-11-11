d50e57399da0e61a805606499dc8d99870b42be5
noah0504789 <noah0504@naver.com>
2023-11-12 02:06:27 +0900

    [feat/dp] 가장 큰 증가하는 부분 수열

---
+++ src/main/java/org/example/dp/가장_큰_증가하는_부분_수열.java
@@ -0,0 +1,42 @@
+package org.example.dp;
+
+// 증가하는 부분 수열 중 합이 가장 큰 것을 리턴하시오.
+
+import java.io.*;
+import java.util.*;
+
+public class 가장_큰_증가하는_부분_수열 {
+
+    static int N;
+    static int[] nums;
+    static int[] prev, dp;
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+
+
+        dp[0] = nums[0];
+
+        for (int i = 1; i < N; i++) {
+            dp[i] = nums[i];
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+nums[i]);
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+
+    private static void init() throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        dp = new int[N];
+        prev = new int[N];
+    }
+}
