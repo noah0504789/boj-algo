01911fd502d705ec44cad6c522a99dc818465832
noah0504789 <noah0504@naver.com>
2023-11-13 00:39:20 +0900

    [feat/backtracking] N과 M (11)

---
+++ src/main/java/org/example/backtracking/N과_M_11.java
@@ -0,0 +1,50 @@
+package org.example.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class N과_M_11 {
+
+    static StringBuffer sb = new StringBuffer();
+    static int N, M;
+    static int[] input, arr, nums;
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        dfs(0);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        int before = 0;
+        for (int i = 0; i < N; i++) {
+            if (nums[i] == before) continue;
+            arr[depth] = nums[i];
+            before = nums[i];
+            dfs(depth+1);
+        }
+    }
+
+    private static void init() throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = input[0];
+        M = input[1];
+        nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
+        arr = new int[M];
+    }
+}
