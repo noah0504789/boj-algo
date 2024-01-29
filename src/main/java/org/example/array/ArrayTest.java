d8bf75319443a82c34749b1cc5ad5023cb019304
noah0504789 <noah0504@naver.com>
2024-01-30 05:26:38 +0900

    1

---
+++ src/main/java/org/example/array/ArrayTest.java
@@ -0,0 +1,26 @@
+package org.example.array;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayTest {
+    static final char[][] cArrs = {
+            {'A', 'B', 'C', 'D', 'E'},
+            {'F', 'G', 'H', 'I', 'J'},
+            {'K', 'L', 'M', 'N', 'O'},
+            {'P', 'Q', 'R', 'S', 'T'}
+    };
+    static final List<Character> vowels = Arrays.asList('A', 'B', 'C', 'D', 'E');
+//    public static void main(String[] args) {
+//        System.out.println(xSearch());
+//    }
+
+//    private static int xSearch() {
+//        for (int i = 0; i < cArrs.length; i++) {
+//            for (int j = 0; j < cArrs[0].length; j++) {
+//                char c = cArrs[i][j];
+//                if (vowels.)
+//            }
+//        }
+//    }
+}
